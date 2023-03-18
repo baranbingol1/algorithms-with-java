e24838bfd06097c16be75cd99f14d659ef0089cf
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-03-18 19:04:06 +0300

    Add files via upload

---
+++ Recursive Solutions/sumOfDigits.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(12));
+    }
+    public static int sumOfDigits(int n){
+        if(n == 0){
+            return 0;
+        } 
+        return n % 10 + sumOfDigits(n/10);
+    }
+}
