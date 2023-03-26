cfb38397822f94e6cf97a9e87ed415efbd10d501
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-03-27 00:15:19 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -21,7 +21,25 @@ public class BinarySearch {
         }
         return -1;
     }
+    public static int BinarySearchRecursively(int[] arr, int target, int start, int end){
+
+        if(start > end)
+            return -1;
+
+        int middle = (start + end)/2;
+
+        if(target == arr[middle])
+            return middle;
+
+        if(target < arr[middle])
+            return BinarySearchRecursively(arr, target, start, middle - 1);
+        else
+            return BinarySearchRecursively(arr, target, middle + 1, end);
+
+
+    }
     public static void main(String[] args) {
+        //Iterative test
         //Binary Search'ün çalışabilmesi için verilen veri yapısının sıralanmış halde olması gerek.
         int[] test = {2,5,7,11,43,89};
         int target = 11;
@@ -30,5 +48,13 @@ public class BinarySearch {
         System.out.println(target + " elemani dizi de " + BinarySearchIteratively(test,target) + ". indekste");
         target = 99;
         System.out.println(target + " elemani dizi de " + BinarySearchIteratively(test,target) + ". indekste");
+        System.out.println("***********************************");
+        //Recursive test
+        target = 11;
+        System.out.println(target + " elemani dizi de " + BinarySearchRecursively(test,target, 0, test.length-1) + ". indekste");
+        target = 2;
+        System.out.println(target + " elemani dizi de " + BinarySearchRecursively(test,target, 0, test.length-1) + ". indekste");
+        target = 99;
+        System.out.println(target + " elemani dizi de " + BinarySearchRecursively(test,target, 0, test.length-1) + ". indekste");
     }
 }
